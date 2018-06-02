572a7c2ef4fc1755288d9b7a4ded825260c88970
dv <suchkov.dv@gmail.com>
2018-06-03 00:20:21 +0300

    release

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+Get all your online friend in [VK](https://vk.com) simply using this tool.
 
 # How to Install
+**Python 3 should be already installed.**
 
-Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+0) Get source code
+```bash
+$ git clone https://github.com/ligain/8_vk_friends_online.git
+```
 
+1) Create virtual environment in the directory where you want to place project.
+```bash
+$ cd 8_vk_friends_online/
+python3 -m venv .env
+```
+
+2) Activate virtual environment
+```bash
+$ . .env/bin/activate
+```
+
+3) Install all dependencies via pip
 ```bash
 pip install -r requirements.txt # alternatively try pip3
 ```
+# How to use
+```bash
+$ cd 8_vk_friends_online/
+python3 vk_friends_online.py
+Enter your VK login: awesome@email.me
+Enter VK password:
 
-Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+Your friends online:
+Brad Pitt
+Johnny Depp
+Julia Roberts
+```
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,53 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6498088
 
 
 def get_user_login():
-    pass
+    return input('Enter your VK login')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass(prompt='Enter VK password: ')
 
 
-def get_online_friends(login, password):
+def connect_vk(login, password, vk_api_version=5.74):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+
+    return vk.API(session, v=vk_api_version, timeout=10)
+
+
+def get_friends_online(vk_api):
+    friends_id_online = vk_api.friends.getOnline()
+    return vk_api.users.get(user_ids=friends_id_online)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if not friends_online:
+        print('There are no friends online')
+        return
+
+    print('\nYour friends online:')
+    for friend in friends_online:
+        print('{} {}'.format(
+            friend.get('last_name'), friend.get('first_name'))
+        )
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+
+    try:
+        vk_api = connect_vk(login, password)
+    except vk.exceptions.VkAuthError as auth_exception:
+        exit('{}. Leaving...'.format(auth_exception))
+
+    friends_online = get_friends_online(vk_api)
     output_friends_to_console(friends_online)
